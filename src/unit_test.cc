ddf75d4d2900579a53867920c76dbe9f7b55b4d1
Younghan Kim <yhkim2778@gmail.com>
2018-09-13 01:20:01 +0900

    test

---
+++ src/unit_test.cc
@@ -0,0 +1,28 @@
+
+#include <cstdio>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+#include "include/hmdgps.h"
+#include "include/geomagnetic.h"
+#include "include/switch.h"
+#include "include/network.h"
+#include "include/hmdcurl.h"
+#include "include/speaker.h"
+#include "include/camera.h"
+
+
+
+
+int main()
+{
+    cout<<"test started!"<<endl;
+
+    
+    return 0;
+}
